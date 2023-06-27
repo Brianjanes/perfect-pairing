f41adf2d1f104b104ceda6b849f20da9425bb6f5
Brian Janes <janes.brian@gmail.com>
2023-06-27 15:00:56 -0230

    front end work

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Content = () => {
+  const [input, setInput] = useState("");
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+    console.log(input);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInput("");
+    console.log(input);
+  };
+
   return (
     <div className="content-main">
       <h2>What would you like to pair wine with?</h2>
       <form className="form">
-        <textarea type="textarea" class="query-input" />
-        <button className="button" type="submit">
+        <textarea
+          type="text"
+          placeholder="Describe your meal."
+          className="query-input"
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+          value={input}
+        />
+        <button
+          className="button"
+          type="submit"
+          onClick={(e) => handleSubmit(e)}
+        >
           Find Pairing
         </button>
       </form>
